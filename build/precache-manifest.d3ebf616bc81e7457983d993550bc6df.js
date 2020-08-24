922cff7f2b8ee9d37344effe233510731631b414
raghavendraks <raghavendraks22@yahoo.com>
2020-08-24 15:47:01 +0100

    update: manifest json

---
+++ build/precache-manifest.d3ebf616bc81e7457983d993550bc6df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "345d8cc7e338e85841e47c7136e73d3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74b38dc84e9d146ddd34",
+    "url": "/static/css/main.9e556004.chunk.css"
+  },
+  {
+    "revision": "95eec90b6abf09b92957",
+    "url": "/static/js/2.cd5dd981.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.cd5dd981.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74b38dc84e9d146ddd34",
+    "url": "/static/js/main.b0d064ee.chunk.js"
+  },
+  {
+    "revision": "5268b1c7be79030bb5b5",
+    "url": "/static/js/runtime-main.142b1330.js"
+  }
+]);
